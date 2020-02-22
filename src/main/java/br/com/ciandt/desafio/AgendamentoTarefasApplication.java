031b23563f33310c533ef94d070a53283f7ffefe
bernardons <bernardo.luks@gmail.com>
2020-02-22 11:45:01 -0300

    initial commit.

---
+++ src/main/java/br/com/ciandt/desafio/AgendamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ciandt.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ciandt/desafio/AgendamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ciandt.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
